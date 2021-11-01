19ea1075581f97980617ff1f176e985963c0b7ee
Fabio_Martinez <rok5150@gmail.com>
2021-11-01 01:53:50 +0000

    Exercise #1

--- main.c
+++ main.c
@@ -3,18 +3,16 @@
 
 int main() {
   
-  int age;
-  double income, height;
-  char gender;
-  char name[50]; //Texto é um vetor de char
+  int age = 20;
+  double income = 5800.0, height = 1.63;
+  char gender = 'F';
+  char name[50] = "Paty"; //Texto é um vetor de char
+  
+  printf("AGE = %d\n", age);
+  printf("INCOME = %.21f\n", income);
+  printf("HEIGHT = %.21f\n", height);
+  printf("GENDER = %c\n", gender);
+  printf("NAME = %s\n", name);
 
-  age = 20;
-  income = 5800.5;
-  height = 1.63;
-  gender = 'F';
-  strcpy(name, "Paty");
-  
-  printf("Age = %d\n", age);
-  
   return 0;
 }
