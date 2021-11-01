5740dc6b3396a3815afc5e55f7ecca6df6823706
Fabio_Martinez <rok5150@gmail.com>
2021-11-01 22:08:39 +0000

    day1 - exercise4

--- main.c
+++ main.c
@@ -1,43 +1,98 @@
 #include <stdio.h>
 #include <string.h>
-void clean_entry() {
-    char c;
-while ((c = getchar()) != '\n' && c != EOF) {} }
-void read_text(char *buffer, int length) {
-    fgets(buffer, length, stdin);
-    strtok(buffer, "\n");
+
+int main() {
+  
+  int x, soma;
+
+  soma = 0;
+  printf("Digite o primeiro numero: ");
+  scanf("%d", &x);
+
+  while (x != 0) {
+    soma = soma + x;
+    printf("Digite o outro numero: ");
+    scanf("%d", &x);
+  }
+  printf("SOMA = %d\n", soma);
+  
+  return 0;
 }
-int main()
-{
-    double salario1, salario2;
-    char nome1[50], nome2[50];
-    int idade;
-    char sexo;
-printf("Nome da primeira pessoa: ");
-read_text(nome1, 50);
-printf("Salario da primeira pessoa: ");
-scanf("%lf", &salario1);
 
-printf("Nome da segunda pessoa: "); 
-clean_entry();
-read_text(nome2, 50);
-printf("Salario da segunda pessoa: "); 
-scanf("%lf", &salario2);
 
-printf("Digite uma idade: "); 
-scanf("%d", &idade); 
-printf("Digite um sexo (F/M): "); 
-clean_entry();
-scanf("%c", &sexo);
 
-printf("Nome 1: %s\n", nome1); 
-printf("Salario 1: %.2lf\n", salario1); 
-printf("Nome 2: %s\n", nome2); 
-printf("Salario 2: %.2lf\n", salario2); 
-printf("Idade: %d\n", idade); 
-printf("Sexo: %c\n", sexo);
 
-return 0; }
+//  _ _ _ _ _ _ _ //
+
+
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main() {
+  
+//   int hour;
+//   printf("Type de time: ");
+//   scanf("%d", &hour);
+
+//   if (hour <12) {
+//     printf("Good morning!\n");
+//   } else {
+//     printf("Good evening\n");
+//   }
+
+//   return 0;
+// }
+
+
+
+
+
+
+// _ _ _ _ _ _ _ //
+
+
+
+// #include <stdio.h>
+// #include <string.h>
+// void clean_entry() {
+//     char c;
+// while ((c = getchar()) != '\n' && c != EOF) {} }
+// void read_text(char *buffer, int length) {
+//     fgets(buffer, length, stdin);
+//     strtok(buffer, "\n");
+// }
+// int main()
+// {
+//     double salario1, salario2;
+//     char nome1[50], nome2[50];
+//     int idade;
+//     char sexo;
+// printf("Nome da primeira pessoa: ");
+// read_text(nome1, 50);
+// printf("Salario da primeira pessoa: ");
+// scanf("%lf", &salario1);
+
+// printf("Nome da segunda pessoa: "); 
+// clean_entry();
+// read_text(nome2, 50);
+// printf("Salario da segunda pessoa: "); 
+// scanf("%lf", &salario2);
+
+// printf("Digite uma idade: "); 
+// scanf("%d", &idade); 
+// printf("Digite um sexo (F/M): "); 
+// clean_entry();
+// scanf("%c", &sexo);
+
+// printf("Nome 1: %s\n", nome1); 
+// printf("Salario 1: %.2lf\n", salario1); 
+// printf("Nome 2: %s\n", nome2); 
+// printf("Salario 2: %.2lf\n", salario2); 
+// printf("Idade: %d\n", idade); 
+// printf("Sexo: %c\n", sexo);
+
+// return 0; }
 
 
 
